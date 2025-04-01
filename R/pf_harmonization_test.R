bb9924a3fccce727ed6238fb26f94f20963df2b1
Thiyaghessan <thiyaghessan@gmail.com>
2025-04-01 21:26:39 +0000

    Refactor and debug first draft

--- R/pf_harmonization_test.R
+++ R/pf_harmonization_test.R
@@ -3,8 +3,12 @@
 library(data.table)
 library(tidyverse)
 library(duckdb)
+library(assertr)
+library(log4r)
 
 source("R/format_ein.R")
+
+my_logger <- create_logger("data/logs/pf_hrmn_log.txt")
 
 # 990 PF crosswalk with old and new variable names, excluding variables already present in the BMF
 xwalk_pf <- rio::import("data/crosswalks/XWALK-990PF-V0.xlsx") |>
@@ -12,44 +16,96 @@ xwalk_pf <- rio::import("data/crosswalks/XWALK-990PF-V0.xlsx") |>
   dplyr::select(VAR_NAME_OLD,
                 VAR_NAME_NEW)
 
-# Load in test PF dataset
-pf_legacy_19 <- data.table::fread(
-  "data/raw/core_pf/CORE-2019-501C3-PRIVFOUND-PF.csv"
-)
+# Iterate across all pf datasets
 
-legacy_names <- toupper(names(pf_legacy_19))
+# Get all PF Dataset list
+core_pf_legacy_ls <- list.files("data/raw/core_pf", full.names = TRUE)
+soi_pf_legacy_ls <- list.files("data/raw/soi_pf", full.names = TRUE)
 
-xwalk_sample <- xwalk_pf |>
-  dplyr::filter(VAR_NAME_OLD %in% legacy_names,
-                ! is.na(VAR_NAME_NEW),
-                VAR_NAME_NEW != "")
+log_df <- data.frame()
 
-unharmonized_cols <- setdiff(legacy_names, xwalk_sample$VAR_NAME_OLD)
+harmonize_pf <- function(path, xwalk_pf, logger){
+  year <- stringr::str_extract(path, "(19|20)\\d{2}")
+  
+  
+  # Read file into data.table object
+  pf_legacy_dt <- data.table::fread(path)
+  setNames(pf_legacy_dt, toupper(names(pf_legacy_dt)))
+  # Get column names
+  legacy_colnames <- names(pf_legacy_dt)
+  # Create xwalk sample
+  xwalk_sample <- xwalk_pf |>
+    dplyr::filter(VAR_NAME_OLD %in% legacy_colnames,
+                  ! is.na(VAR_NAME_NEW),
+                  VAR_NAME_NEW != "")
+  # Get columnn names that are absent
+  unharmonized_cols <- setdiff(legacy_colnames, xwalk_sample$VAR_NAME_OLD)
+  # Perform harmonization
+  harmonizable_cols <- unique(xwalk_sample$VAR_NAME_OLD)
+  pf_legacy_sample <- pf_legacy_dt[, ..harmonizable_cols]
+  data.table::setnames(pf_legacy_sample,
+                       xwalk_sample$VAR_NAME_OLD,
+                       xwalk_sample$VAR_NAME_NEW
+  )
+  # Duplicated column names - need to log
+  dup_columns <- names(pf_legacy_sample)[duplicated(names(pf_legacy_sample))]
+  # Create tax year column and stop if not there
+#  stopifnot("F9_00_TAX_PERIOD_END_DATE |TAX_PERIOD not found" = "F9_00_TAX_PERIOD_END_DATE" %in% names(pf_legacy_sample | "TAX_PERIOD" %in% names(pf_legacy_sample)))
+#  pf_legacy_sample |> assertr::verify(has_all_names("F9_00_TAX_PERIOD_END_DATE"))
+  if ("F9_00_TAX_PERIOD_END_DATE" %in% harmonizable_cols){
+    pf_legacy_sample[, TAX_YEAR := substr(F9_00_TAX_PERIOD_END_DATE, 1, 4)]
+  } else {
+    pf_legacy_sample[, TAX_YEAR := substr(TAX_PERIOD, 1, 4)]
+  }
+  # Create EIN2 Column with check
+  pf_legacy_sample[, EIN2 := format_ein(F9_00_ORG_EIN, to="id")]
+  # stopifnot("Not all EINs have 14 characters" = unique(nchar(pf_legacy_sample$EIN2)) != 14)
+  # Log outputs
+  log4r::info(logger, message = paste("Year", year))
+#  log4r::info(logger, message = paste("Unharmonized cols:", paste0(unharmonized_cols, collapse = ", ")))
+#  log4r::info(logger, message = paste("Harmonized cols:", paste0(harmonizable_cols, collapse = ", ")))
+  log4r::info(logger, message = paste("Duplicated cols:", paste0(dup_columns, collapse = ", ")))
+  
+  return(pf_legacy_sample)
+}
+
+harmonize_pf(core_pf_legacy_ls[[10]], xwalk_pf, my_logger)
+
+rs <- purrr::map(.x = core_pf_legacy_ls,
+           .f = harmonize_pf,
+           xwalk_pf = xwalk_pf,
+           logger = my_logger,
+           .progress = TRUE)
+
+
+soi_rs <- purrr::map(.x = soi_pf_legacy_ls,
+                 .f = harmonize_pf,
+                 xwalk_pf = xwalk_pf,
+                 logger = my_logger,
+                 .progress = TRUE)
+
+# TODO: RENAME DUPLICATES, EXTRACT YEAR FOR PF FILES
+
+
+core_pf_legacy_ls[[10]]
+
+xwalk_pf |> dplyr::filter(VAR_NAME_OLD == "P6EXMPF")
+
 
 # Add step to log the unharmonized columns in a separate table
   # Name, Fiscal Year, URL to relevant form 990
 # Add step to log the names and existing data types of the columns in the data
 
 # harmonization
-harmonizable_cols <- unique(xwalk_sample$VAR_NAME_OLD)
-pf_legacy_19_sample <- pf_legacy_19[, ..harmonizable_cols]
 
-data.table::setnames(
-  pf_legacy_19_sample,
-  xwalk_sample$VAR_NAME_OLD,
-  xwalk_sample$VAR_NAME_NEW
-)
 
-# Duplicated column names - need to log before running it all over again
-dup_columns <- names(pf_legacy_19_sample)[duplicated(names(pf_legacy_19_sample))]
+
 
 # Create tax year and put it all into a duckdb database
-pf_legacy_19_sample[, TAX_YEAR := substr(F9_00_TAX_PERIOD_END_DATE, 1, 4)]
 
 # Create EIN2 column
-pf_legacy_19_sample[, EIN2 := format_ein(F9_00_ORG_EIN, to="id")]
 # check for 14 characters 
-table(nchar(pf_legacy_19_sample$EIN2))
+table()
 
 # Put into a DuckDB database
 con <- dbConnect(duckdb::duckdb(), dbdir = "data/pf_hrmn.db")

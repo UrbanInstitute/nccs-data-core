2a9f3039e58b611bd7dc50f133621f97465961ed
Thiyaghessan <thiyaghessan@gmail.com>
2025-04-02 05:54:01 +0000

    Test run of pf harmonization final draft

--- R/pf_harmonization_test.R
+++ R/pf_harmonization_test.R
@@ -7,6 +7,7 @@ library(assertr)
 library(log4r)
 
 source("R/format_ein.R")
+source("R/data-dictionary_helpers.R")
 
 my_logger <- create_logger("data/logs/pf_hrmn_log.txt")
 
@@ -22,24 +23,29 @@ xwalk_pf <- rio::import("data/crosswalks/XWALK-990PF-V0.xlsx") |>
 core_pf_legacy_ls <- list.files("data/raw/core_pf", full.names = TRUE)
 soi_pf_legacy_ls <- list.files("data/raw/soi_pf", full.names = TRUE)
 
-log_df <- data.frame()
-
 harmonize_pf <- function(path, xwalk_pf, logger){
-  year <- stringr::str_extract(path, "(19|20)\\d{2}")
   
+  # Extract the calendar year for the legacy core/soi dataset
+  year <- stringr::str_extract(path, "(19|20)\\d{2}")
+  if (is.na(year)){year <- paste0("20", stringr::str_extract(path, "\\d{2}"),
+                                  collapse = "")}
   
   # Read file into data.table object
   pf_legacy_dt <- data.table::fread(path)
   setNames(pf_legacy_dt, toupper(names(pf_legacy_dt)))
+  
   # Get column names
   legacy_colnames <- names(pf_legacy_dt)
+  
   # Create xwalk sample
   xwalk_sample <- xwalk_pf |>
     dplyr::filter(VAR_NAME_OLD %in% legacy_colnames,
                   ! is.na(VAR_NAME_NEW),
                   VAR_NAME_NEW != "")
+  
   # Get columnn names that are absent
   unharmonized_cols <- setdiff(legacy_colnames, xwalk_sample$VAR_NAME_OLD)
+  
   # Perform harmonization
   harmonizable_cols <- unique(xwalk_sample$VAR_NAME_OLD)
   pf_legacy_sample <- pf_legacy_dt[, ..harmonizable_cols]
@@ -47,176 +53,167 @@ harmonize_pf <- function(path, xwalk_pf, logger){
                        xwalk_sample$VAR_NAME_OLD,
                        xwalk_sample$VAR_NAME_NEW
   )
+  
   # Duplicated column names - need to log
   dup_columns <- names(pf_legacy_sample)[duplicated(names(pf_legacy_sample))]
-  # Create tax year column and stop if not there
-#  stopifnot("F9_00_TAX_PERIOD_END_DATE |TAX_PERIOD not found" = "F9_00_TAX_PERIOD_END_DATE" %in% names(pf_legacy_sample | "TAX_PERIOD" %in% names(pf_legacy_sample)))
-#  pf_legacy_sample |> assertr::verify(has_all_names("F9_00_TAX_PERIOD_END_DATE"))
-  if ("F9_00_TAX_PERIOD_END_DATE" %in% harmonizable_cols){
-    pf_legacy_sample[, TAX_YEAR := substr(F9_00_TAX_PERIOD_END_DATE, 1, 4)]
+  
+  # Create column containing tax year
+  if ("F9_00_FISCAL_YEAR_END" %in% harmonizable_cols){
+    pf_legacy_sample[, TAX_YEAR := substr(F9_00_FISCAL_YEAR_END, 1, 4)]
   } else {
-    pf_legacy_sample[, TAX_YEAR := substr(TAX_PERIOD, 1, 4)]
+    pf_legacy_sample[, TAX_YEAR := substr(F9_00_TAX_PERIOD_END_DATE, 1, 4)]
   }
+  
   # Create EIN2 Column with check
   pf_legacy_sample[, EIN2 := format_ein(F9_00_ORG_EIN, to="id")]
-  # stopifnot("Not all EINs have 14 characters" = unique(nchar(pf_legacy_sample$EIN2)) != 14)
+  
   # Log outputs
   log4r::info(logger, message = paste("Year", year))
-#  log4r::info(logger, message = paste("Unharmonized cols:", paste0(unharmonized_cols, collapse = ", ")))
-#  log4r::info(logger, message = paste("Harmonized cols:", paste0(harmonizable_cols, collapse = ", ")))
+  log4r::info(logger, message = paste("Unharmonized cols:", paste0(unharmonized_cols, collapse = ", ")))
+  log4r::info(logger, message = paste("Harmonized cols:", paste0(harmonizable_cols, collapse = ", ")))
   log4r::info(logger, message = paste("Duplicated cols:", paste0(dup_columns, collapse = ", ")))
   
   return(pf_legacy_sample)
 }
 
-harmonize_pf(core_pf_legacy_ls[[10]], xwalk_pf, my_logger)
+pf_ls <- c(core_pf_legacy_ls, soi_pf_legacy_ls)
+pf_hrmn_ls <- purrr::map(
+  .x = pf_ls,
+  .f = harmonize_pf,
+  xwalk_pf = xwalk_pf,
+  logger = my_logger,
+  .progress = TRUE
+)
 
-rs <- purrr::map(.x = core_pf_legacy_ls,
-           .f = harmonize_pf,
-           xwalk_pf = xwalk_pf,
-           logger = my_logger,
-           .progress = TRUE)
-
-
-soi_rs <- purrr::map(.x = soi_pf_legacy_ls,
-                 .f = harmonize_pf,
-                 xwalk_pf = xwalk_pf,
-                 logger = my_logger,
-                 .progress = TRUE)
-
-# TODO: RENAME DUPLICATES, EXTRACT YEAR FOR PF FILES
-
-
-core_pf_legacy_ls[[10]]
-
-xwalk_pf |> dplyr::filter(VAR_NAME_OLD == "P6EXMPF")
-
-
-# Add step to log the unharmonized columns in a separate table
-  # Name, Fiscal Year, URL to relevant form 990
-# Add step to log the names and existing data types of the columns in the data
-
-# harmonization
-
-
-
-
-# Create tax year and put it all into a duckdb database
-
-# Create EIN2 column
-# check for 14 characters 
-table()
-
-# Put into a DuckDB database
+# Put all datasets into a DuckDB database
+pf_hrmn_dt <- data.table::rbindlist(pf_hrmn_ls, fill = TRUE)
 con <- dbConnect(duckdb::duckdb(), dbdir = "data/pf_hrmn.db")
-
-duckdb::dbWriteTable(con, "pf_hrmn", pf_legacy_19_sample, overwrite = TRUE)
+duckdb::dbWriteTable(con,
+                     "pf_hrmn",
+                     pf_hrmn_dt,
+                     overwrite = TRUE)
 
 # Get schema and data types
 schema <- dbGetQuery(con, "PRAGMA table_info(pf_hrmn)")
 
-# Get tax years in table
-tax_years <- dbGetQuery(con,
-           "SELECT DISTINCT TAX_YEAR FROM pf_hrmn ORDER BY TAX_YEAR DESC")
-tax_years
+# Export to parquet
 
-# write to a parquet file
-
-# Select a specific tax year and deduplicated data
-pf_2020 <- dbGetQuery(con,
-           "SELECT DISTINCT * FROM pf_hrmn WHERE TAX_YEAR = '2020'")
-
-# remove columns with all NA
-columns <- names(pf_2020)
-column_stats <- dbGetQuery(con, paste0(
-  "SELECT ", 
-  paste(sprintf("COUNT(%s) AS %s", columns, columns), collapse = ", "),
-  " FROM pf_hrmn"
-)) |>
-  tidyr::pivot_longer(cols = everything(),
-                      names_to = "VAR_NAME_NEW",
-                      values_to = "NUM_OBS_NOT_NA")
-
-na_cols <- column_stats |>
-  dplyr::filter(NUM_OBS_NOT_NA == 0) |>
-  dplyr::pull(VAR_NAME_NEW)
-
-# Drop columns with all NA
-
-if (length(na_cols) > 0) {
-  for (col in na_cols){
-    dbExecute(con, paste0("ALTER TABLE pf_hrmn DROP COLUMN ", col))
+# Add in metadata
+harmonized_vars <- schema$name
+## unharmonized columns
+## years for each variable
+coverages <- c()
+for (var in harmonized_vars){
+  years <- dbGetQuery(con,
+                    sprintf("SELECT TAX_YEAR FROM pf_hrmn 
+                    WHERE %s IS NOT NULL 
+                    GROUP BY TAX_YEAR 
+                    ORDER BY TAX_YEAR DESC", var)) |>
+    unlist()
+  if (length(years) != 0){
+    coverage <- format_years_range(years)
+  } else {
+    coverage <- "None"
   }
+  coverages <- c(coverages, coverage)
+}
+schema$coverage <- coverages
+## percentage of NA values
+na_percents <- c()
+record_count <- dbGetQuery(con, "SELECT COUNT(*) as count FROM pf_hrmn")
+for (var in harmonized_vars){
+  na_count <- dbGetQuery(con,
+                      sprintf("SELECT COUNT(*) as null_count FROM pf_hrmn WHERE %s IS NULL", var)) |>
+    unlist()
+  na_percent <- na_count / record_count * 100
+  na_percents <- c(na_percents, na_percent)
 }
 
-# Write the results out
+schema$na_percent <- unlist(na_percents)
 
-dbExecute(con, "COPY pf_hrmn TO 'data/pf_test.csv' (HEADER, DELIMITER ',', QUOTE '\"')")
+# Cardinality
+cardinality <- c()
+for (var in harmonized_vars){
+  cardinality_percent <- dbGetQuery(con, sprintf(
+    "SELECT COUNT(DISTINCT %s) * 100.0 / COUNT(%s) AS cardinality_ratio
+     FROM pf_hrmn",
+    var,
+    var
+  )) |> unlist()
+  cardinality <- c(cardinality, cardinality_percent)
+}
+schema$cardinality_percent <- cardinality
 
-# save metadata
-# number of rows, colu
+# Save schema
+data.table::fwrite(schema, "data/schemas/PF-HRMN-SCHEMA-V0.csv")
 
-# end steps
-# deduplication
-
-# SCHEMA
-
-# DATASET = 3 COLUMNS {var_name: INT, year: INT, url: STR}
-# SQL = CREATE TABLE missing_vars (var_name INT, year INT, url STR)
-
-# Rename columns to match crosswalk
-
-var_rename_df <- xwalk_pf |>
-  dplyr::mutate(var_old = toupper(var_old),
-                var_new = toupper(var_new)) |>
-  filter(var_old %in% toupper(names(pf_legacy_19)),
-         var_new != "IRS_SUBSECTOR_CODE",
-         var_new != "")
-
-data.table::setnames(pf_legacy_19, 
-                     var_rename_df$var_old, 
-                     var_rename_df$var_new)
-
-missing_vars <- setdiff(names(pf_legacy_19), var_rename_df$var_new)
-
-pf_legacy_19[, (missing_vars) := NULL]
-
-# tax year
-names <- c("column_a", "column_a")
-newnames <- make.unique(names(pf_legacy_19))
-names(pf_legacy_19) <- newnames
-
-# we need to save other tax years separately
-pf_hrmn_19 <- pf_legacy_19 |>
-  dplyr::mutate(TAX_YEAR_END = as.character(TAX_YEAR_END)) |>
-  dplyr::filter(TAX_YEAR_END == "2019")
-
-table(pf_hrmn_19$TAX_YEAR_END)
-
-# ein - format ein function copy/paste - to create EIN2
-pf_hrmn_19$F9_00_ORG_EIN[1:10]
-
-# data types?
-column_types <- sapply(pf_hrmn_19, class)
-
-# log what's missing
+# Deduplicate
+dbExecute(con, "
+  CREATE OR REPLACE TABLE pf_hrmn AS
+  SELECT DISTINCT *
+  FROM pf_hrmn
+")
 
 
+# write to a parquet file
+dbExecute(con, "COPY pf_hrmn TO 'data/processed/pf/full/CORE-FULL-501C3-PRIVFOUND-PF-HRMN-V0.parquet' (FORMAT PARQUET)")
 
-# post edit of what's missing
+# Select a specific tax year and deduplicated data
 
-data.table::fwrite(pf_hrmn_19, 
-                   "data/processed/CORE-2019-501C3-PF-HRMN_V0.csv")
+# Get tax years in table
+tax_years <- dbGetQuery(con,
+                        "SELECT DISTINCT TAX_YEAR FROM pf_hrmn ORDER BY TAX_YEAR DESC")
 
+# Save individual data marts
 
-# data type - SQL Database
-  # save to .csv on website
-  # create SQL database - .parqut, 
-  # Preferred SQL database - Postgres, MYSQL
-      # SQLite - not recommended for large datasets
-      # DUCKDB
-# data quality assessments
+for (year in tax_years$TAX_YEAR){
+  message(paste("Processing", year))
+  filename <- sprintf("CORE-%s-501C3-PRIVFOUND-PF-HRMN-V0.csv", year)
+  filepath <- paste0("data/processed/pf/full/", filename, collapse = "")
+  pf <- dbGetQuery(con,
+                   sprintf("SELECT DISTINCT * FROM pf_hrmn 
+                            WHERE TAX_YEAR = '%s'", year))
+  data.table::fwrite(pf, filepath)
+}
 
+# Create data dictionary
+concordance <- rio::import("data/concordance/CONCORDANCE-PF-V0.xlsx")
+concordance <- concordance |>
+  dplyr::select(
+    variable_name,
+    label,
+    description,
+    form,
+    form_type,
+    form_part,
+    form_line_number
+  ) |>
+  dplyr::rename_with(
+    toupper
+  ) |>
+  dplyr::rename(
+    VAR_NAME_NEW = VARIABLE_NAME
+  )
 
+dd <- schema |>
+  dplyr::select(
+    name,
+    type,
+    na_percent,
+    coverage,
+    cardinality_percent
+  ) |>
+  dplyr::rename(
+    VAR_NAME_NEW = name,
+    VAR_DATA_TYPE = type,
+    VAR_NA_PERCENT = na_percent,
+    VAR_COVERAGE = coverage,
+    VAR_CARDINALITY_PERCENT = cardinality_percent
+  ) |>
+  tidylog::left_join(
+    concordance
+  )
 
-test_df
+data.table::fwrite(dd, "data/dd/DD-PF-HRMN-V0.csv")
+
+View(dd)

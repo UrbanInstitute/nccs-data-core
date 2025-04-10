ae3de1ed1acdcc6cfe86954bbfef6b920007a5e5
thiyaghessan <thiyaghessan@gmail.com>
2025-04-10 17:03:30 +0000

    Update file

--- R/core_update.R
+++ R/core_update.R
@@ -7,6 +7,7 @@ library(paws)
 library(purrr)
 library(stringr)
 library(rio)
+library(duckdb)
 
 # Helper Scripts
 source("R/utils.R")
@@ -32,7 +33,7 @@ soi_updt_url_ls <- soi_url_ls[grepl("(22|23).*?(990\\.xlsx|ez\\.xlsx)$", soi_url
 
 # Download - need to debug
 
-download_raw_data(url_ls = soi_updt_url_ls,
+download_raw_data(url_ls = soi_updt_url_ls[3:4],
                   destfolder = "data/raw/soi/",
                   logger = my_logger)
 
@@ -99,6 +100,7 @@ rename_soi_990ez <- function(soi_dt, xwalk_990ez, logger){
   
 }
 
+soi_ls <- purrr::map(soi_ls, data.table::fread)
 
 soi_hrmn_ls <- purrr::map(
   soi_ls,
@@ -111,25 +113,201 @@ soi_hrmn_ls <- purrr::map(
 
 # C3 - PC Scope
 
-c3_pc <- purrr::map(
-  list.files("data/harmonized/core/501c3-pc/", full.names = TRUE),
+c3_pc_old <- purrr::map(
+  list.files("data/harmonized/core/501c3-pc/", full.names = TRUE)[1:11],
   data.table::fread
+) |>
+  data.table::rbindlist(fill = TRUE)
+
+c3_pc_new_ls <- list(
+  soi_hrmn_ls[[1]],
+  soi_hrmn_ls[[3]]
 )
 
-c3_pc_new <- data.table::rbindlist(
-  list(
-    soi_hrmn_ls[[1]],
-    soi_hrmn_ls[[3]]
-  )
-) |>
+c3_pc_new <- data.table::rbindlist(c3_pc_new_ls) |>
   dplyr::filter(
     F9_00_SUBSECTION_CODE == 3
   ) |>
   dplyr::distinct()
 
-c3_pc_full <- data.table::rbindlist(
-  c(c3_pc, c3_pc_new),
-  fill = TRUE
+cols_to_exclude <- c(
+  "F9_00_TAX_PERIOD_BEGIN_DATE",
+  "F9_00_TAX_YEAR",
+  "F9_03_PROG_CODE",
+  "F9_03_PROG_CODE_2",
+  "F9_03_PROG_CODE_3",
+  "F9_08_REV_PROG_BIZCODE",
+  "F9_08_REV_PROG_BIZCODE_2",
+  "F9_08_REV_PROG_BIZCODE_3",
+  "F9_08_REV_PROG_BIZCODE_4",
+  "F9_08_REV_PROG_BIZCODE_5",
+  "F9_09_EXP_OTH_OTH_TOT",
+  "F9_10_ASSET_LAND_BLDG_DEPREC",
+  "GEO_ZIP5",
+  "MISSION_NTEE",
+  "DUP_RTRN_X",
+  "F9_00_GROUP_EXEMPT_NUM",
+  "F9_00_RETURN_TYPE",
+  "F9_00_ORG_ADDR_CITY",
+  "F9_00_ORG_ADDR_L1",
+  "F9_00_ORG_ADDR_STATE",
+  "F9_00_ORG_ADDR_ZIP",
+  "F9_00_ORG_NAME_DBA_L1",
+  "F9_00_ORG_NAME_L1"
 )
 
+c3_pc_old <- c3_pc_old |>
+  dplyr::select(!any_of(cols_to_exclude)) |>
+  dplyr::rename(
+    "F9_00_SUBSECTION_CODE" = BMF_SUBSECTION_CODE,
+    "F9_08_REV_MISC_BIZCODE_A" = F9_08_REV_MISC_BIZCODE,
+    "F9_08_REV_MISC_BIZCODE_B" = F9_08_REV_MISC_BIZCODE_2,
+    "F9_08_REV_MISC_BIZCODE_C" = F9_08_REV_MISC_BIZCODE_3,
+    "F9_08_REV_MISC_TOT_A" = F9_08_REV_MISC_TOT,
+    "F9_08_REV_MISC_TOT_B" = F9_08_REV_MISC_TOT_2,
+    "F9_08_REV_MISC_TOT_C" = F9_08_REV_MISC_TOT_3,
+    "F9_08_REV_PROG_DESC_A" = F9_08_REV_PROG_DESC,
+    "F9_08_REV_PROG_DESC_B" = F9_08_REV_PROG_DESC_2,
+    "F9_08_REV_PROG_DESC_C" = F9_08_REV_PROG_DESC_3,
+    "F9_08_REV_PROG_DESC_D" = F9_08_REV_PROG_DESC_4,
+    "F9_08_REV_PROG_DESC_E" = F9_08_REV_PROG_DESC_5,
+    "F9_08_REV_PROG_TOT_A" = F9_08_REV_PROG_TOT,
+    "F9_08_REV_PROG_TOT_B" = F9_08_REV_PROG_TOT_2,
+    "F9_08_REV_PROG_TOT_C" = F9_08_REV_PROG_TOT_3,
+    "F9_08_REV_PROG_TOT_D" = F9_08_REV_PROG_TOT_4,
+    "F9_08_REV_PROG_TOT_E" = F9_08_REV_PROG_TOT_5,
+    "F9_09_EXP_OTH_TOT_A" = F9_09_EXP_OTH_TOT,
+    "F9_09_EXP_OTH_TOT_B" = F9_09_EXP_OTH_TOT_2,
+    "F9_09_EXP_OTH_TOT_C" = F9_09_EXP_OTH_TOT_3,
+    "F9_09_EXP_OTH_TOT_D" = F9_09_EXP_OTH_TOT_4,
+    "F9_09_EXP_OTH_TOT_E" = F9_09_EXP_OTH_TOT_5,
+    "F9_09_EXP_OTH_TOT_F" = F9_09_EXP_OTH_TOT_6
+  )
 
+c3_pc_full <- list(c3_pc_old, c3_pc_new)
+
+c3_pc_full <- data.table::rbindlist(c3_pc_full, fill = TRUE) |>
+  dplyr::distinct()
+
+# Import into duckdb
+
+con <- dbConnect(duckdb(), dbdir = "my_database.duckdb")
+dbWriteTable(con, "c3_pc", c3_pc_full, overwrite = TRUE)
+
+# Get tax years
+tax_years <- dbGetQuery(con,
+                        "SELECT DISTINCT TAX_YEAR FROM c3_pc ORDER BY TAX_YEAR DESC")
+tax_years
+# Get schema and data types
+schema <- dbGetQuery(con, "PRAGMA table_info(c3_pc)")
+
+# Add in metadata
+harmonized_vars <- schema$name
+## unharmonized columns
+## years for each variable
+coverages <- c()
+for (var in harmonized_vars){
+  years <- dbGetQuery(con,
+                      sprintf("SELECT TAX_YEAR FROM c3_pc 
+                    WHERE %s IS NOT NULL 
+                    GROUP BY TAX_YEAR 
+                    ORDER BY TAX_YEAR DESC", var)) |>
+    unlist()
+  if (length(years) != 0){
+    coverage <- format_years_range(years)
+  } else {
+    coverage <- "None"
+  }
+  coverages <- c(coverages, coverage)
+}
+schema$coverage <- coverages
+## percentage of NA values
+na_percents <- c()
+record_count <- dbGetQuery(con, "SELECT COUNT(*) as count FROM c3_pc")
+for (var in harmonized_vars){
+  na_count <- dbGetQuery(con,
+                         sprintf("SELECT COUNT(*) as null_count FROM c3_pc WHERE %s IS NULL", var)) |>
+    unlist()
+  na_percent <- na_count / record_count * 100
+  na_percents <- c(na_percents, na_percent)
+}
+
+schema$na_percent <- unlist(na_percents)
+
+# Cardinality
+cardinality <- c()
+for (var in harmonized_vars){
+  cardinality_percent <- dbGetQuery(con, sprintf(
+    "SELECT COUNT(DISTINCT %s) * 100.0 / COUNT(%s) AS cardinality_ratio
+     FROM c3_pc",
+    var,
+    var
+  )) |> unlist()
+  cardinality <- c(cardinality, cardinality_percent)
+}
+schema$cardinality_percent <- cardinality
+
+# Save schema
+
+data.table::fwrite(schema, "data/schemas/C3_PC-HRMN-SCHEMA-V1.csv")
+
+# Deduplicate
+dbExecute(con, "
+  CREATE OR REPLACE TABLE c3_pc AS
+  SELECT DISTINCT *
+  FROM c3_pc
+")
+
+
+# write to a parquet file
+dbExecute(con, "COPY c3_pc TO 'data/harmonized/core/501c3-pc/full/CORE-FULL-501C3-CHARITIES-PC-HRMN-V1.parquet' (FORMAT PARQUET)")
+
+# Save individual data marts
+
+for (year in tax_years$TAX_YEAR){
+  message(paste("Processing", year))
+  filename <- sprintf("CORE-%s-501C3-CHARITIES-PC-HRMN-V1.csv", year)
+  filepath <- paste0("data/harmonized/core/501c3-pc/marts/", filename, collapse = "")
+  pf <- dbGetQuery(con,
+                   sprintf("SELECT DISTINCT * FROM c3_pc 
+                            WHERE TAX_YEAR = '%s'", year))
+  data.table::fwrite(pf, filepath)
+}
+
+# Create data dictionary
+concordance <- rio::import("data/concordance/CONCORDANCE-PF-V0.xlsx")
+concordance <- concordance |>
+  dplyr::select(
+    variable_name,
+    label,
+    description,
+    form,
+    form_type,
+    form_part,
+    form_line_number
+  ) |>
+  dplyr::rename_with(
+    toupper
+  ) |>
+  dplyr::rename(
+    VAR_NAME_NEW = VARIABLE_NAME
+  )
+
+dd <- schema |>
+  dplyr::select(
+    name,
+    type,
+    na_percent,
+    coverage,
+    cardinality_percent
+  ) |>
+  dplyr::rename(
+    VAR_NAME_NEW = name,
+    VAR_DATA_TYPE = type,
+    VAR_NA_PERCENT = na_percent,
+    VAR_COVERAGE = coverage,
+    VAR_CARDINALITY_PERCENT = cardinality_percent
+  ) |>
+  tidylog::left_join(
+    concordance
+  )

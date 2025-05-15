facc81db810f951e447f6456dd773abe84bb1bdb
Thiyaghessan <thiyaghessan@gmail.com>
2025-05-15 13:59:45 -0400

    Add TODO and debug getting list of files

--- R/01_data_harmonize.R
+++ R/01_data_harmonize.R
@@ -1,78 +1,93 @@
 # Script Header
-# Description: This script contains the harmonization code for CORE Harmonization 
+# Description: This script standardizes the variable names across both the Legacy CORE files and the SOI files.
 # Programmer: Thiyaghessan Poongundranar - tpoongundranar@urban.org
 # Date Created: 2024-07-25
-# Date Last Edited: 2025-02-04
+# Date Last Edited: 2025-05-01
 # Details:
 # (1) - Load Crosswalks
 # (2) - Run Harmonization
-
-# Load Packages
-library(data.table)
-library(purrr)
-library(tidyverse)
-library(log4r)
-
-# Load Helper Scripts
-source("R/data_harmonize_helpers.R")
-source("R/utils.R")
 
 # Set up Logging
 my_logger <- create_logger("data/logs/data_harmonize_log.txt")
 
 # (1) Load Crosswalks
 
-# (1.1) SOI
-XWALK_SOI <- readxl::read_xlsx("data/crosswalks/VARIABLE-NAME-CROSSWALK-V1.xlsx")
-
-harmonize_cols <-
-  c(
-    "EFILE.VAR",
-    "SOI.VAR",
-    "YEAR.2021.ez",
-    "YEAR.2021.pc",
-    "YEAR.2020.ez",
-    "YEAR.2020.pc",
-    "YEAR.2019.ez",
-    "YEAR.2019.pc",
-    "YEAR.2018.ez",
-    "YEAR.2018.pc",
-    "YEAR.2017.ez",
-    "YEAR.2017.pc",
-    "YEAR.2016.ez",
-    "YEAR.2016.pc"
-  )
-
-XWALK_SOI <- XWALK_SOI %>% 
-  dplyr::select(tidyselect::all_of(harmonize_cols)) %>% 
-  tidyr::pivot_longer(! EFILE.VAR,
-                      names_to = "Series",
-                      values_to = "var_old",
-                      values_drop_na = TRUE) %>% 
-  dplyr::rename("var_new" = "EFILE.VAR") %>% 
-  dplyr::select(! Series) %>% 
-  dplyr::mutate(var_old = toupper(var_old)) %>% 
+# (1.1) 990 and 990EZ crosswalk
+xwalk_990 <- data.table::fread("data/crosswalks/XWALK-990-V2.csv") |>
+  dplyr::filter(
+    VAR_NAME_NEW != "NOT IN CONCORDANCE",
+    VAR_NAME_NEW != "NOT IN DD"
+  ) |>
   dplyr::distinct()
 
-# SOI - PF
-pf_xwalk <- rio::import("data/crosswalks/pf_crosswalk.xlsx")
+# (1.2) 990PF crosswalk
+xwalk_990PF <- rio::import("data/crosswalks/XWALK-990PF-V0.xlsx") |>
+  dplyr::filter(is.na(FOUND_IN_BMF)) |>
+  dplyr::select(VAR_NAME_OLD,
+                VAR_NAME_NEW)
 
-pf_xwalk <- pf_xwalk |>
-  dplyr::select(OlD, NEW) |>
-  dplyr::rename(var_old = OlD,
-                var_new = NEW)
+# (2) Harmonize datasets - Standardize variable names across datasets
 
-# (1.2) CORE
-XWALK_CORE <- readr::read_csv( "data/crosswalks/VAR-CROSSWALK-CORE-COMBINED.csv" )
+# (2.1) 501C3-CHARITIES-PC
+legacy_501c3pc_file_ls <- get_files(folder_name = "data/raw/core/", 
+                                    scope = "501C3-CHARITIES-PC")
+soi_990ez_file_ls <- get_files(folder_name = "data/raw/soi/", 
+                               scope = "ez\\.xlsx")
+pc_501c3_file_ls <- c(legacy_501c3pc_file_ls, soi_990ez_file_ls)
 
-# (2) Run Harmonization
+# Extract
+extract_filings <- function(file_path){
+  dat <- data.table::fread(file_path)
+}
+# Transform
+harmonize_filings <- function(dat, xwalk_df, logger, file_path){
+  # Get necessary metadata
+  legacy_colnames <- names(dat)
+  file_name <- tools::file_path_sans_ext(file_path)
+  # Harmonize columns
+  xwalk_df <- xwalk_df |> dplyr::filter(VAR_NAME_OLD %in% legacy_colnames)
+  harmonizable_cols <- xwalk_df$VAR_NAME_OLD
+  dat_hrmn <- dat[, ..harmonizable_cols]
+  data.table::setnames(dat_hrmn, 
+                       old = harmonizable_cols, 
+                       new = xwalk_df$VAR_NAME_NEW)
+  # Log unharmonized columns
+  unharmonized_cols <- setdiff(legacy_colnames, harmonizable_cols)
+  log4r::info(logger,
+              message = paste(
+                "File:",
+                file_name,
+                "-",
+                "Unharmonized cols:",
+                paste0(unharmonized_cols, collapse = ", ")
+              ))
+  
+  return(dat_hrmn)
+}
+create_ein2 <- function(dat){
+  dat[, EIN2 := format_ein(EIN, to = "n")]
+  return(dat)
+}
+create_taxyear <- function(dat, tax_year_column) {
+  dat[, TAX_YEAR := as.character(substr(get(tax_year_column), 1, 4))]
+  return(dat)
+}
+# Load
+write_to_database <- function(dat, path, table_name){
+  db_name <- paste0(path, table_name, ".duckdb")
+  con <- DBI::dbConnect(duckdb::duckdb(), dbdir = db_name)
+  DBI::dbWriteTable(con, table_name, dat, append = TRUE)
+  DBI::dbDisconnect(con)
+}
 
-# Core
+# Create Schema
+
+# After SOI do partitioning and then create new schema
 
 # (2.1) Get list of raw files
 
 # PC and PZ
-raw_core_501c3_pc_files_ls <- get_files( folder_name = "data/raw/core/", scope = "501C3-CHARITIES-PC" )
+
 raw_core_501c3_pz_files_ls <- get_files( folder_name = "data/raw/core/", scope = "501C3-CHARITIES-PZ" )
 raw_core_501ce_pc_files_ls <- get_files( folder_name = "data/raw/core/", scope = "501CE-NONPROFIT-PC" )
 raw_core_501ce_pz_files_ls <- get_files( folder_name = "data/raw/core/", scope = "501CE-NONPROFIT-PZ" )
@@ -196,3 +211,6 @@ pf_processed <- data.frame(pf_lazy)
 # save everything by year
 
 
+# TODO:
+# Validation check: Unharmonized columns
+# Consistent Tax Year Column
